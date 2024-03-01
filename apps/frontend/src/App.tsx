fb8032ac7a5c5882279601d802a24eaead6bcb9f
Samuel Deotti <samuel_deotti@hotmail.com>
2024-03-01 15:53:40 -0300

    adding sign up component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
-// import SignUp from './pages/SignUp/SignUp';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      {/* <Route path="/sign-up" element={ <SignUp /> } /> */}
+      <Route path="/sign-up" element={ <SignUp /> } />
     </Routes>
   );
 }
