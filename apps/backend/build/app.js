8ad1049ede1ea0adf6ced5c154f972f0496c7920
Samuel Deotti <samuel_deotti@hotmail.com>
2024-03-04 18:57:07 -0300

    changing the build

--- apps/backend/build/app.js
+++ apps/backend/build/app.js
@@ -3,38 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = exports.App = void 0;
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-require("express-async-errors");
-const routes_1 = __importDefault(require("./routes"));
-const errorMiddleware_1 = __importDefault(require("./middlewares/errorMiddleware"));
-class App {
-    constructor() {
-        this.app = (0, express_1.default)();
-        this.config();
-        this.app.get('/', (req, res) => res.json({ ok: true }));
-        this.routes();
-        this.app.use(errorMiddleware_1.default);
-    }
-    routes() {
-        this.app.use(routes_1.default);
-    }
-    config() {
-        const accessControl = (_req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-            res.header('Access-Control-Allow-Headers', '*');
-            next();
-        };
-        this.app.use(express_1.default.json());
-        this.app.use(accessControl);
-        this.app.use((0, cors_1.default)());
-    }
-    start(PORT) {
-        this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
-    }
-}
-exports.App = App;
-// Essa segunda exportação é estratégica, e a execução dos testes de cobertura depende dela
-exports.app = new App().app;
+const login_routes_1 = __importDefault(require("./routes/login.routes"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use('/login', login_routes_1.default);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+exports.default = app;
--- apps/backend/build/server.js
+++ apps/backend/build/server.js
@@ -1,5 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.APP_PORT || 5432;
-new app_1.App().start(PORT);
+const app_1 = __importDefault(require("./app"));
+const port = process.env.PORT || 5432;
+app_1.default.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
